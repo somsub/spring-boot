b0fdb2894463962a8640bff1e34ef3fdb77da98e
Somsubhra Ghosh <ssghos1@monsanto.com>
2017-03-31 17:21:40 -0500

    Learnt Crud Repository from Spring Data JPA

--- course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -4,42 +4,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("java", "Core Java", "Core Java Course"),
-			new Topic("spring", "Core Spring", "Core Spring Course"),
-			new Topic("javascript", "JavaScipt and JQuery", "JavaScript Course"),
-			new Topic("scala", "Scala Fundamentals", "Scala Fundamentals Course")));
-
+	@Autowired
+	private TopicRepository topicRepository;
+	
 	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<Topic>();
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 
 	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findOne(id);
 	}
 
 	public void insertTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
 
 	public void deleteTopic(String id) {
-		Topic topic = topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-		if (topic != null) {
-			topics.remove(topic);
-		}
+		topicRepository.delete(id);
 	}
 
 	
 	public void updateTopic(Topic topic) {
-		Topic topicInList = topics.stream().filter(t -> t.getId().equals(topic.getId())).findFirst().get();
-		if (topicInList != null) {
-			topics.remove(topicInList);
-			topics.add(topic);
-		}
-		
+		topicRepository.save(topic);		
 	}
 
 }
